25352b295f110a0a93f2673b623f73de5a07ab7f
Orlando Neto <orlandoneto23@gmail.com>
2024-04-12 11:34:27 -0300

    first commit

---
+++ test/test_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'rspec/rails'
+
+if ENV['RAILS_ENV'] ||= 'test'
+  require 'simplecov'
+  SimpleCov.start
+  puts 'required simplecov'
+end
+
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
+end
