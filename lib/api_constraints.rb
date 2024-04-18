9e83f49836d555130907bdc8a783d6b80ee35373
Orlando Neto <orlandoneto23@gmail.com>
2024-04-18 14:56:15 -0300

    Modified file test on rspec.

---
+++ lib/api_constraints.rb
@@ -0,0 +1,11 @@
+class ApiConstraints
+    def initialize(options)
+      @version = options[:version]
+      @default = options[:default]
+    end
+  
+    def matches?(request)
+      @default || request.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
+    end
+  end
+  
